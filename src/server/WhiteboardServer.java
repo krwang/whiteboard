70415d41d587673195d46842b5c197d8c1dfb2da
Kelly Liu <kelyliu@mit.edu>
2013-12-10 23:36:31 -0500

    whiteboard server username token

--- src/server/WhiteboardServer.java
+++ src/server/WhiteboardServer.java
@@ -173,18 +173,16 @@ public class WhiteboardServer {
 	 * open takes in whiteboard name and username
 	 * boardName must be valid already when this is called
 	 * handles the client's input and returns the corresponding output
-<<<<<<< HEAD
-	 * puts out moves, boardname
-=======
 	 * 
->>>>>>> 9f9f10f5679c48e1ee5e711b7207e3b5d79d3a2e
+	 * puts out moves, boardname
 	 * @param input
 	 * @return
 	 */
 	private Object[] handleRequest(String input, Socket socket) {
 		System.out.println("handleRequest");
 		System.out.println("input: " + input);
-		String regex = "(add \\w+ \\w+)|(draw \\d+ \\d+ \\d+ \\d+ \\d+ \\d+ \\w+)|(bye \\w+ \\w+)";
+		String regex = "(add \\w+ \\w+)|(draw \\d+ \\d+ \\d+ \\d+ \\d+ \\d+ \\w+)|(bye \\w+ \\w+)|"
+				+ "(username \\w+)";
 //		Object[] output;
 //		Canvas canvas;
 		if ( ! input.matches(regex)) {
@@ -263,7 +261,6 @@ public class WhiteboardServer {
 			System.out.println("draw y2 " + y2);
 
 			String boardName = tokens[7];
-			System.out.println("boardname " + boardName);
 //			canvas = canvasMap.get(boardName);
 //			System.out.println("canvas name " + canvas);
 //			canvas.drawLineSegment(color,size, x1,y1,x2,y2);
@@ -281,12 +278,28 @@ public class WhiteboardServer {
 			canvasMovesMap.put(boardName, pastCanvasMoves);
 			
 			return new Object[]{pastCanvasMoves, boardName};
+			
 		} else if(tokens[0].equals("bye")) {
 			System.out.println("bye");
 //			String userName = tokens[1];
 			String boardName = tokens[2];
-			return new Object[]{input, boardName};
-		} else {
+			String username = tokens[1];
+			usernames.remove(username);
+			return new Object[]{new ArrayList<String>().add(input), boardName};
+			
+		} else if(tokens[0].equals("username")){
+			System.out.println("username");
+			String username = tokens[1];
+			if(username.isEmpty()){
+				return new Object[]{"unavailable"};
+			}
+			else if(usernames.contains(username)){
+				return new Object[]{"contains"};
+			}
+			return new Object[]{"username good"};
+			//UGHHHHHHH I DONT GET THISSSSSSSSSSS
+		}
+		else {
 			throw new UnsupportedOperationException();
 		}		
 	}
